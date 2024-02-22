f650afcc2791d5264f090181aed46a4a65dc88b0
Kaung Myat San <kaungmyatsan.cse@gmail.com>
2024-02-22 23:36:13 +0000

    defining a function to resize the images provided in the dataset on cnn.py in the directory of learning models

--- learning-models/cnn.py
+++ learning-models/cnn.py
@@ -19,3 +19,15 @@ from keras.callbacks import EarlyStopping
 from keras.utils import np_utils
 from sklearn.metrics import log_loss
 from keras import __version__ as keras_version
+
+# defining a function to resize the images provided in the dataset
+# Parameters
+# ----------
+# img_path : path
+#     path of the image to be resized
+def resize_image(img_path):
+    # reading image file
+    img = cv2.imread(img_path)
+    # Resize the image to be 32 by 32
+    img_resized = cv2.resize(img, (32, 32), cv2.INTER_LINEAR)
+    return img_resized
