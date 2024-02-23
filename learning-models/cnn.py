a41d0b934773c0036cb769392d941e490b6b303d
Kaung Myat San <kaungmyatsan.cse@gmail.com>
2024-02-23 00:07:38 +0000

    defining a function to use load_testing_samples() for loading and resizing testing samples on cnn.py in the directory of learning models

--- learning-models/cnn.py
+++ learning-models/cnn.py
@@ -78,3 +78,26 @@ def load_normalize_training_samples():
     # Calling the load function in order to load and resize the training samples
     training_samples, training_label, training_sample_id = load_training_samples()
     # Converting the loaded and resized data into Numpy format
+    training_samples = np.array(training_samples, dtype=np.uint8)
+    training_label = np.array(training_label, dtype=np.uint8)
+    # Reshaping the training samples
+    training_samples = training_samples.transpose((0, 3, 1, 2))
+    # Converting the training samples and training labeles into float format
+    training_samples = training_samples.astype('float32')
+    training_samples = training_samples / 255
+    training_label = np_utils.to_categorical(training_label, 8)
+    return training_samples, training_label, training_sample_id
+
+# defining a function to use load_testing_samples() for loading and resizing testing samples.
+def load_normalize_testing_samples():
+    # Calling the load function in order to load and resize the testing samples
+    testing_samples, testing_samples_id = load_testing_samples()
+    # Converting the loaded and resized data into Numpy format
+    testing_samples = np.array(testing_samples, dtype=np.uint8)
+    # Reshaping the testing samples
+    testing_samples = testing_samples.transpose((0, 3, 1, 2))
+    # Converting the testing samples into float format
+    testing_samples = testing_samples.astype('float32')
+    testing_samples = testing_samples / 255
+    return testing_samples, testing_samples_id
+    
