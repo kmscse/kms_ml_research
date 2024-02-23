2752949c3da628556c2ae0bd398d07b15aa78a56
Kaung Myat San <kaungmyatsan.cse@gmail.com>
2024-02-23 00:50:50 +0000

    defining a function with the trained CNN models as an input to test them using test dataset on cnn.py in the directory of learning models

--- learning-models/cnn.py
+++ learning-models/cnn.py
@@ -186,3 +186,33 @@ def create_model_with_kfold_cross_validation(nfolds=10):
     #Reporting the model loss at this stage
     overall_settings_output_string = 'loss_' + str(overall_score) + '_folds_' + str(nfolds) + '_ep_' + str(num_epochs)
     return overall_settings_output_string, trained_models 
+
+# defining a function with the trained CNN models as an input to test them using test dataset
+def test_generality_crossValidation_over_test_set( overall_settings_output_string, cnn_models):
+    batch_size = 16 # in each iteration, we consider 32 training examples at once
+    fold_number = 0 # fold iterator
+    def merge_several_folds_mean(yfull_test, number_of_folds):
+        # Implementation of the merge_several_folds_mean function
+        # ...
+        pass
+
+    number_of_folds = len(cnn_models) # Creating number of folds based on the value used in the training step
+    def format_results_for_types(test_result, testing_samples_id, overall_settings_output_string):
+        # Implementation of the format_results_for_types function
+        # ...
+        pass
+
+    yfull_test = [] # variable to hold overall predictions for the test set
+    # executing the actual cross validation test process over the test set
+    for j in range(number_of_folds):
+        model = cnn_models[j]
+        fold_number += 1
+        print('Fold number {} out of {}'.format(fold_number, number_of_folds))
+        # Loading and normalizing testing samples
+        testing_samples, testing_samples_id = load_normalize_testing_samples()
+        # Calling the current model over the current test fold
+        test_prediction = model.predict(testing_samples, batch_size=batch_size, verbose=2)
+        yfull_test.append(test_prediction)
+    test_result = merge_several_folds_mean(yfull_test, number_of_folds)
+    overall_settings_output_string = 'loss_' + overall_settings_output_string + '_folds_' + str(number_of_folds)
+    format_results_for_types(test_result, testing_samples_id, overall_settings_output_string)
